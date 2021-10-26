93a89751a49d8e7f6166c09c5c3e55b9b4d2e3be
a-robbins <45301147+a-robbins@users.noreply.github.com>
2021-10-26 16:46:00 -0500

    GUI - Initial Commit

---
+++ RobbinsMessagingSystem/src/GUIDriver.java
@@ -0,0 +1,25 @@
+
+import javax.swing.JDialog;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author a-a-robbins
+ */
+public class GUIDriver {
+     public static void main(String[] args) {
+       //might want to add a class to the GUI for the person
+       
+       //create a new homepage frame (which everything else will be generated from)
+        UserHomepageFrame homePage = new UserHomepageFrame();
+        homePage.setVisible(true);
+
+    }
+    
+    
+}
